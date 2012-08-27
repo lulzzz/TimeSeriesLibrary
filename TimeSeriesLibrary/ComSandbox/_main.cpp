37736f420bd35116d1ebdd5048256e70f0a8f2ab
tpulokas <tpulokas@hydrologics.net>
2012-08-27 13:02:28 -0700

    fiddle with ComSandbox

--- TimeSeriesLibrary/ComSandbox/_main.cpp
+++ TimeSeriesLibrary/ComSandbox/_main.cpp
@@ -1,14 +1,18 @@
 #include "StdAfx.h"
 
 #import "mscorlib.tlb"
-#import "..\bin\Release\TimeSeriesLibrary.tlb"
+#import "C:\OASIS\libs\TimeSeriesLibrary.tlb"
 
 using namespace TimeSeriesLibrary;
 
 void main()
 {
     //CoInitialize(NULL);
-
     //TimeSeriesLibrary::ComTSLibrary __uuidof(TimeSeriesLibrary::ComTSLibrary);
+
+    TimeSeriesLibrary::_ComTSLibrary *TSLib;
+    CoInitialize(NULL);
+    TimeSeriesLibrary::_ComTSLibraryPtr p(__uuidof(TimeSeriesLibrary::ComTSLibrary));
+    TSLib = p;
 
 }
